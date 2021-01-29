df2e52b0a147b9de99e06e27fa1dbdeaeafe4535
Kaustubh <saikaustubh10@gmail.com>
2021-01-29 11:15:46 +0530

    added singleproject component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project=project />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import SingleProject from '../components/SingleProject.vue'
 
 export default {
   name: 'Home',
-  components: {
-    
+  components: { SingleProject },
+  data(){
+    return{
+      projects: []
+    }
+  },
+  mounted(){
+    fetch("http://localhost:3000/projects")
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
   }
 }
 </script>
